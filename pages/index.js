6012e39fbaaabf067e95795605a5d26e9c87b4d1
heypurnomo <heypurnomo@gmail.com>
2022-06-11 11:00:47 +0700

    feat: slider

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import { Box } from '@mui/material'
 import ListProduct from '../components/ListProduct'
 import Layout from '../components/Layout'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, EffectCreative, Pagination } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/effect-creative'
 
 export async function getStaticProps() {
   const { rows: list1 } = await (
@@ -8,10 +14,7 @@ export async function getStaticProps() {
   ).json()
   const { rows: list2 } = await (
     await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/product?limit=6&orderBy=createdAt`,
-      {
-        credentials: 'include',
-      }
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/product?limit=6&orderBy=createdAt`
     )
   ).json()
 
@@ -34,7 +37,56 @@ export default function Home({ list1, list2 }) {
             maxHeight: '100vw',
             backgroundColor: '#30694D',
           }}
-        ></Box>
+        >
+          <Swiper
+            slidesPerView={1}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            effect={'creative'}
+            creativeEffect={{
+              prev: {
+                shadow: true,
+                translate: ['-20%', 0, -1],
+              },
+              next: {
+                translate: ['100%', 0, 0],
+              },
+            }}
+            modules={[Autoplay, Pagination, EffectCreative]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <Box
+                sx={{
+                  position: 'relative',
+                  width: '100%',
+                  height: '510px',
+                  backgroundColor: '#B7D0B1',
+                }}
+              >
+                slide1
+              </Box>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box
+                sx={{
+                  position: 'relative',
+                  width: '100%',
+                  height: '510px',
+                  backgroundColor: '#316D4D',
+                }}
+              >
+                slide3
+              </Box>
+            </SwiperSlide>
+          </Swiper>
+        </Box>
         <Box>
           <ListProduct title="Bagaimana Dengan Ini" products={list1} />
         </Box>
